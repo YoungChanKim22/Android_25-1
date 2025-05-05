e5983c5edb464fb1600c48edb945f93eedc27c31
CHAN\chan0 <young030717@naver.com>
2025-05-05 13:57:55 +0900

    assignment05

--- app/src/main/java/activity.kt
+++ app/src/main/java/activity.kt
@@ -1,2 +1,49 @@
-class activity {
+package com.example.bcsd_android_2025_1
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Activity : AppCompatActivity() {
+    var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val num_textview : TextView = findViewById(R.id.textview_num)
+        val toast_button : Button = findViewById(R.id.button_toast)
+        val count_button : Button = findViewById(R.id.button_count)
+        val random_button : Button = findViewById(R.id.button_random)
+
+        toast_button.setOnClickListener {
+            Toast.makeText(this, "상진인 애기야!", Toast.LENGTH_SHORT).show()
+        }
+
+        count_button.setOnClickListener {
+            count++
+            num_textview.text = count.toString()
+        }
+
+        random_button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("currentCount", count)
+            startActivityForResult(intent, 7777)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 7777 && resultCode == RESULT_OK) {
+            val result = data?.getIntExtra("randomNumber", count) ?: count
+            count = result
+            val numTextView: TextView = findViewById(R.id.textview_num)
+            numTextView.text = count.toString()
+        }
+    }
 }
--- app/src/main/java/activitysecond.kt
+++ app/src/main/java/activitysecond.kt
@@ -1,2 +1,33 @@
 package com.example.bcsd_android_2025_1
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+import kotlinx.coroutines.delay
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val max = intent.getIntExtra("currentCount",0)
+        val randomValue = Random.nextInt(0, max + 1)
+
+        val resultText : TextView= findViewById(R.id.textview_random)
+        resultText.text = "랜덤 숫자: $randomValue"
+
+        lifecycleScope.launch {
+            delay(1000L)
+            val resultIntent = Intent()
+            resultIntent.putExtra("randomNumber", randomValue)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
